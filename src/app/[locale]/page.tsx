e9645038afba139a6819fe7599a843b4c6454b56
shuhay70 <shuhei0210aaa@icloud.com>
2025-10-19 19:40:43 +0900

    fix size of top

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,33 +3,40 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div className="items-center bg-[#505938] h-screen">
-      <div className="flex flex-col h-[500px] pt-[120px] justify-between">
+    <div className="flex flex-col items-center bg-[#505938] h-screen">
+      <Image
+        className="lg:pl-[20px] lg:w-auto lg:h-auto my-[30px]"
+        src="/image/logo.jpg"
+        width={120}
+        height={120}
+        alt="おこげのロゴ"
+      ></Image>
+      <div className="flex flex-col w-full mb-[50px]">
         <Link
           href="/menu"
           locale="ja"
-          className="text-center text-2xl bg-white text-black mx-[40px] py-[35px] rounded-md hover:bg-white/20 transition-colors mb-[50px]"
+          className="text-center text-2xl bg-white text-black mx-[40px] py-[25px] rounded-md hover:bg-white/20 transition-colors mb-[20px]"
         >
           日本語
         </Link>
         <Link
           href="/menu"
           locale="en"
-          className="text-center text-2xl bg-white text-black mx-[40px] py-[35px] rounded-md hover:bg-white/20 transition-colors mb-[50px]"
+          className="text-center text-2xl bg-white text-black mx-[40px] py-[25px] rounded-md hover:bg-white/20 transition-colors mb-[20px]"
         >
           ENGLISH
         </Link>
         <Link
           href="/menu"
           locale="zh"
-          className="text-center text-2xl bg-white text-black mx-[40px] py-[35px] rounded-md hover:bg-white/20 transition-colors mb-[50px]"
+          className="text-center text-2xl bg-white text-black mx-[40px] py-[25px] rounded-md hover:bg-white/20 transition-colors mb-[20px]"
         >
           中文
         </Link>
         <Link
           href="/menu"
           locale="ko"
-          className="text-center text-2xl bg-white text-black mx-[40px] py-[35px] rounded-md hover:bg-white/20 transition-colors mb-[50px]"
+          className="text-center text-2xl bg-white text-black mx-[40px] py-[25px] rounded-md hover:bg-white/20 transition-colors mb-[20px]"
         >
           한국어
         </Link>
