2e9ea0eb4006b8145f4e9d94be7e278416ddc88b
shuhay70 <shuhei0210aaa@icloud.com>
2025-11-28 02:59:09 +0900

    アンケート機能実装後の修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,17 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+export default function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  
+  // adminページは国際化の対象外に。
+  if (pathname.startsWith('/admin')) {
+    return NextResponse.next();
+  }
+  
+  return createMiddleware(routing)(request);
+}
 
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
